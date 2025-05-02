d9c898bd63850beec8c88b92d027de40f32bc5f4
Your Name <your.email@example.com>
2025-05-02 05:52:01 -0400

    Initial setup with Firebase and CloudKit

---
+++ DesignView.swift
@@ -0,0 +1,18 @@
+//
+//  DesignView.swift
+//  PrecisionFlowAI
+//
+//  Created by Summit Precision Homes on 5/2/25.
+//
+
+import SwiftUI
+
+struct DesignView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DesignView()
+}
